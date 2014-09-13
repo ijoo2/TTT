853e35542668b6f85bed1db251e2f94c475d50dd
ijoo2 <inhajoo9@gmail.com>
2014-09-12 22:43:45 -0500

    TTT
    Tic Tac Toe with an unbeatable AI (WIP).

---
+++ TTT.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <array>
+
+using namespace std;
+
+class TicTacToe
+{
+    public:
+        TicTacToe();
+    private:
+        int board[3][3];
+        int player;
+        char displayBoard[3][3];
+
+}
+
+TicTacToe::TicTacToe()
+{
+    player = 1;
+    for(int i=0; i<3; i++){
+        for(int j=-; j<3; j++){
+            board[i][j] = 0;
+            displayBoard[i][j] = ' ';
+            
+}
+
+
+
+int main()
+{
+    
+    TicTacToe game;
+    bool more = true;
+
+    while(more){
+
+    
+
+    return 0;
+}
